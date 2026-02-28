7516df79be44d56cfc9e4569576f1aecb4f20d0b
wTapioca <mateusts2013@gmail.com>
2026-02-27 21:55:28 -0300

    Added all the files to the repository.

---
+++ Calculator/Index/JS/Index.js
@@ -0,0 +1,81 @@
+// Funções
+
+// Função do botão "Clear"
+function Clear() {
+    console.log("Cleared")
+}
+
+// Função do botão "Dot"
+function AddDot() {
+    console.log("Added a dot")
+}
+
+// Função do botão "Solve"
+function Solve() {
+    console.log("Solved")
+}
+
+// Função do botão "Divide"
+function Clear() {
+    console.log("Divided")
+}
+
+// Função do botão "Seven"
+function Clear() {
+    console.log("Cleared")
+}
+
+// Função do botão "Eight"
+function Clear() {
+    console.log("Cleared")
+}
+
+// Função do botão "Nine"
+function Clear() {
+    console.log("Cleared")
+}
+
+// Função do botão "Multiply"
+function Clear() {
+    console.log("Cleared")
+}
+
+// Função do botão "Four"
+function Clear() {
+    console.log("Cleared")
+}
+
+// Função do botão "Five"
+function Clear() {
+    console.log("Cleared")
+}
+
+// Função do botão "Six"
+function Clear() {
+    console.log("Cleared")
+}
+
+// Função do botão "Minus"
+function Clear() {
+    console.log("Cleared")
+}
+
+// Função do botão "One"
+function Clear() {
+    console.log("Cleared")
+}
+
+// Função do botão "Two"
+function Clear() {
+    console.log("Cleared")
+}
+
+// Função do botão "Three"
+function Clear() {
+    console.log("Cleared")
+}
+
+// Função do botão "Plus"
+function Clear() {
+    console.log("Cleared")
+}
---
+++ Calculator/Misc/ReadMe.txt
@@ -0,0 +1,6 @@
+Olá, essa é uma calculadora basica que fiz para melhorar
+meus conhecimentos sobre HTML e CSS e aprender JavaScript.
+Nenhuma IA ou tutorial foram usados para fazer essa
+calculadora, a não ser que você conte pesquisar o que
+algumas tags fazem. Obrigado por ler, e se sinta livre
+para editar e copiar este código!
