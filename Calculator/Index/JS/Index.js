03f76f71dd605e78a505c9ca50db58a9766b96be
wTapioca <mateusts2013@gmail.com>
2026-02-27 22:08:22 -0300

    Updated the files to the latest version

--- Calculator/Index/JS/Index.js
+++ Calculator/Index/JS/Index.js
@@ -1,81 +1,9 @@
-// Funções
+// Achar o display
 
-// Função do botão "Clear"
-function Clear() {
-    console.log("Cleared")
-}
+const Display = document.getElementById(display)
 
-// Função do botão "Dot"
-function AddDot() {
-    console.log("Added a dot")
-}
+// Funções dos botões de numero
 
-// Função do botão "Solve"
-function Solve() {
-    console.log("Solved")
-}
-
-// Função do botão "Divide"
-function Clear() {
-    console.log("Divided")
-}
-
-// Função do botão "Seven"
-function Clear() {
-    console.log("Cleared")
-}
-
-// Função do botão "Eight"
-function Clear() {
-    console.log("Cleared")
-}
-
-// Função do botão "Nine"
-function Clear() {
-    console.log("Cleared")
-}
-
-// Função do botão "Multiply"
-function Clear() {
-    console.log("Cleared")
-}
-
-// Função do botão "Four"
-function Clear() {
-    console.log("Cleared")
-}
-
-// Função do botão "Five"
-function Clear() {
-    console.log("Cleared")
-}
-
-// Função do botão "Six"
-function Clear() {
-    console.log("Cleared")
-}
-
-// Função do botão "Minus"
-function Clear() {
-    console.log("Cleared")
-}
-
-// Função do botão "One"
-function Clear() {
-    console.log("Cleared")
-}
-
-// Função do botão "Two"
-function Clear() {
-    console.log("Cleared")
-}
-
-// Função do botão "Three"
-function Clear() {
-    console.log("Cleared")
-}
-
-// Função do botão "Plus"
-function Clear() {
-    console.log("Cleared")
+function AddSeven() {
+    return 0
 }
